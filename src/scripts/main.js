a9546a74269598bf71faef1f283db57621ba5f49
Satanpit <alex.hyrenko@gmail.com>
2016-07-07 13:01:01 +0300

    Styling responsive menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
-    var menu = document.querySelector('.menu');
+    var menu = document.querySelector('.menu'),
+        menuToggle = document.querySelector('.menu-toggle');
 
-    document.querySelector('.menu-toggle').addEventListener('click', function (e) {
+    menuToggle.addEventListener('click', function (e) {
         menu.classList.toggle('active');
+        menuToggle.classList.toggle('active');
     });
 
     hljs.initHighlightingOnLoad();
